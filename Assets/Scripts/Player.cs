0b9c9bf4a608f4ddb82c189d52d57052d9a69dc7
ghofranakil <ghofran.akil@gmail.com>
2018-05-24 16:15:22 +0200

    add all files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	public Text countText;
+	public Text winText;
+	Transform lastCheckpoint;
+	private int count;
+
+	void Start () {
+
+		count = 0;
+		SetCountText();
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Collectable Object")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+			winText.text = "";
+		}
+
+		if (other.gameObject.CompareTag ("Dying Plane")) {
+			if (lastCheckpoint == null) {
+				SceneManager.LoadScene ("Jumping game");
+			} else {
+				// go to last checkpoint
+				Debug.Log("Respawn at last checkpoint:" + lastCheckpoint.position);
+				Rigidbody rb = GetComponent<Rigidbody> ();
+				rb.velocity = Vector3.zero;
+				rb.angularVelocity = Vector3.zero;
+				transform.position = lastCheckpoint.position;
+			}
+
+		}
+
+		if (other.gameObject.CompareTag ("Checkpoint")) {
+			lastCheckpoint = other.gameObject.transform;
+			Debug.Log("Checkpoint passed: " + other.name + " " + lastCheckpoint.position);
+		}
+
+		if (other.gameObject.CompareTag ("Winning Collectable Object")) {
+			other.gameObject.SetActive (false);
+			winText.text = "BRAVO! \nYou just won a useless game";
+		}
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString();
+	}
+}
